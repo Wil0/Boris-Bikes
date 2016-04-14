dfc29671989b31b78ee0ea7b7bcb7c2fc4eff460
Emilio <emsmnndz@gmail.com>
2016-04-14 13:57:28 +0100

    Updated tests to support doubles

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,14 +5,15 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to(:list_bikes) } #what step is this?
 
+let(:bike) {double :bike}
   describe "#release_bike" do
     it "releases a docked bike" do
-      bike = Bike.new
+      allow(bike).to receive(:working?).and_return(true)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
     it "releases a working bike" do
-      bike = Bike.new
+      allow(bike).to receive(:working?).and_return(true)
       subject.dock(bike)
       expect(subject.release_bike).to be_working
     end
@@ -20,8 +21,9 @@ describe DockingStation do
       expect { subject.release_bike }.to raise_error "No bikes available"
     end
     it "should not release a broken bike" do
+      allow(bike).to receive(:working?).and_return(false)
+      allow(bike).to receive(:report_broken).and_return(false)
       station = DockingStation.new
-      bike = Bike.new
       bike.report_broken
       station.dock(bike)
       expect {station.release_bike}.to raise_error "No working bikes available"
@@ -30,29 +32,27 @@ describe DockingStation do
 
   describe "#dock" do
     it "docks the bike" do
-      bike = Bike.new
       expect(subject.dock(bike)).to eq [bike]
     end
     it "docks a broken bike" do
-      bike = Bike.new
+      allow(bike).to receive(:report_broken).and_return(false)
       bike.report_broken
       expect(subject.dock(bike)).to eq [bike]
     end
     it "does not allow you to dock a bike when the dock is full" do
-      DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
-      expect { subject.dock(Bike.new) }.to raise_error "The dock is full"
+      DockingStation::DEFAULT_CAPACITY.times {subject.dock(double :bike)}
+      expect { subject.dock(double :bike) }.to raise_error "The dock is full"
     end
     it "does not allow you to dock a bike when the dock, with a custom capacity, is full" do
       station = DockingStation.new(50)
-      subject.capacity.times {subject.dock(Bike.new)}
-      expect { subject.dock(Bike.new) }.to raise_error "The dock is full"
+      subject.capacity.times {subject.dock(double :bike)}
+      expect { subject.dock(double :bike) }.to raise_error "The dock is full"
     end
 
   end
 
   describe "#bike" do #when do you create this describe test
     it "returns docked bikes" do
-      bike = Bike.new
       subject.dock(bike)
       expect(subject.list_bikes).to eq [bike]
     end
