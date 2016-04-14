3226131e5911b101b2910400645e67f56ebf0650
Alex Robertson <lex1989@gmail.com>
2016-04-14 16:14:59 +0100

    Added van pickup and drop off tests

--- lib/van.rb
+++ lib/van.rb
@@ -1,9 +1,26 @@
 #pickup bikes
 #release bikes
 class Van
-  attr_reader :capacity
+  attr_reader :capacity, :bikes
+
   DEFAULT_CAPACITY = 5
+
   def initialize
     @capacity = DEFAULT_CAPACITY
+    @bikes = []
+    @location = "Garage"
   end
+
+  def pickup(bike)
+    @bikes << bike
+  end
+
+  def release_bike
+    @bikes.pop
+  end
+
+  def travel
+    @location == "Garage" ? location = "Docking Station" : location = "Garage"
+  end
+
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -5,20 +5,42 @@ describe Van do
     expect(subject.capacity).to eq Van::DEFAULT_CAPACITY
   end
   let(:bike) {double :bike}
-    describe "#release_bike" do
-      it "releases a bike" do
+    describe "#pickup" do
+      it "picks up a bike" do
         subject.pickup(bike)
-        expect(subject.release_bike).to eq bike
+        expect(subject.bikes).to eq [bike]
       end
-      it "releases a working bike to the dock station" do
+      it "only picks up broken bikes at the docking station" do
+        allow(bike).to receive(:working?).and_return(true)
+        subject.travel
+        if bike.working? == true
+          expect { subject.pickup(bike) }.to raise_error "Bike is not broken"
+        end
+      end
+      it "only picks up working bikes at the garage" do
+        allow(bike).to receive(:report_broken).and_return(false)
+        bike.report_broken
+        expect { subject.pickup(bike) }.to raise_error "Bike is broken"
+      end
+
+
+    end
+    describe "#release" do
+      it "releases a bike" do
         allow(bike).to receive(:working?).and_return(true)
         subject.pickup(bike)
         subject.travel
-        subject.release_bike
-        station = DockingStation.new
-        station.dock(bike)
-        expect(station.release_bike).to be_working
+        expect(subject.release_bike).to eq bike
+      end
+      it "only releases working bikes at the docking station" do
+        allow(bike).to receive(:report_broken).and_return(false)
+        subject.travel
+        bike.report_broken
+        expect { subject.release_bike}.to raise_error "Bike is broken"
+      end
+      it "only releases broken bikes at the garage" do
+        subject.pickup(bike)
+        expect { subject.release_bike }.to raise_error "Bike is working"
       end
     end
-
 end
